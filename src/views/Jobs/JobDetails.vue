1714037c8595c7346413ff70caf8c1e2817601b2
= <=>
2022-03-31 08:34:16 -0500

    completed

--- src/views/Jobs/JobDetails.vue
+++ src/views/Jobs/JobDetails.vue
@@ -1,11 +1,27 @@
 <template>
-  <h1>Job Details Page</h1>
-  <p>The job id is {{ id }}</p>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <p>The job id is {{ id }}</p>
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Loading in the details...</p>
+  </div>
 </template>
 
 <script>
 export default {
-  props: ["id"]
+  props: ["id"],
+  data() {
+    return {
+      job: null
+    }
+  },
+  async mounted() {
+    const res = await fetch("http://localhost:5000/jobs/" + this.id)
+    const data = await res.json()
+    this.job = data
+  }
   // data() {
   //   return {
   //     id: this.$route.params.id
--- src/views/Jobs/JobsView.vue
+++ src/views/Jobs/JobsView.vue
@@ -1,9 +1,14 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
-    <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
-      <h2>{{ job.title }}</h2>
-    </router-link>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="job">
+      <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+        <h2>{{ job.title }}</h2>
+      </router-link>
+    </div>
+  </div>
+  <div v-else>
+    <p>Preparing the list...</p>
   </div>
 </template>
 
@@ -15,7 +20,7 @@ export default {
     }
   },
   async mounted() {
-    const res = await fetch("http://localhost:3000/jobs")
+    const res = await fetch("api/jobs")
     const data = await res.json()
     this.jobs = data
   }
